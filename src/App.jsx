fd708f5cbec82c96a290f753220bb3cce3e4d6d1
Thibault Gleize <tibo.gleize@gmail.com>
2025-05-04 12:50:04 +0200

    Ajout Banner CTA + modification header taille logo et suppression nom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './styles/App.scss'
 import Header from './features/Header/Header'
+import Banner from './features/Banner/Banner'
 
 function App() {
   return (
-    <Header /> 
-    
+    <>
+      <Header /> 
+      <Banner />
+    </>
   )
 }
 
---
+++ src/features/AboutMe/AboutMe.jsx
@@ -0,0 +1,2 @@
+import './aboutme.scss';
+import portrait from '../../assets/images/AI4A8823 - Mars 2018.jpg';
