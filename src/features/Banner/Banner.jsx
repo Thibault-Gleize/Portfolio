447f2ebdc345e294896aa316ed804daf77e7a840
Thibault Gleize <tibo.gleize@gmail.com>
2025-05-07 20:35:20 +0200

    Ajout aspect responsive Banner

--- src/features/Banner/Banner.jsx
+++ src/features/Banner/Banner.jsx
@@ -3,15 +3,14 @@ import './banner.scss';
 
 export default function Banner () {
 
-    const names = ["Thibault Gleize", "Intégrateur web", "Designer Web", ]
-
     return (
         <section className='banner'>
             <h2>
-                <span className='banner-change'>Hello World &#x1F30D;
+                Hello World &#x1F30D;
                 <br/>
-                Je suis {names[0]}, 
-                </span> Du code sur mesure pour votre projet, avec la passion d'un junior et la rigueur d'un pro !
+                Je suis Thibault Gleize, 
+                <br/>
+                Du code sur mesure pour votre projet, avec la passion d'un junior et la rigueur d'un pro !
             </h2>
             <button>Apprenez en plus sur moi &#x270C;</button>
         </section>
