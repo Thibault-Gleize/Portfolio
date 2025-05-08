614be7a70022042b93e1258524efc6df104f1c32
Thibault Gleize <tibo.gleize@gmail.com>
2025-05-08 15:38:36 +0200

    Correction couleur copyright footer

--- src/features/Footer/Footer.jsx
+++ src/features/Footer/Footer.jsx
@@ -7,8 +7,8 @@ export default function Footer () {
         <footer>
             <p className='copyright'>&#169; 2025 Thibault Gleize. All right reserved</p>
             <div className='social-media'>
-                <a href="https://www.linkedin.com/in/thibault-gleize-1530a92aa/"><img className='social-logo' src={linkedIn_logo} alt="Accès à mon Linked-In" /></a>
-                <a href="https://github.com/Thibault-Gleize/"><img className='social-logo' src={github_logo} alt="Accès à mon Github" /></a>
+                <a target='_blank' href="https://www.linkedin.com/in/thibault-gleize-1530a92aa/"><img className='social-logo' src={linkedIn_logo} alt="Accès à mon Linked-In" /></a>
+                <a target='_blank' href="https://github.com/Thibault-Gleize/"><img className='social-logo' src={github_logo} alt="Accès à mon Github" /></a>
             </div>
         </footer>
     )
